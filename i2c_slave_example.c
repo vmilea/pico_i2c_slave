bcc425aa9b05dc6663cd6ff5a68fe1d23950b7fd
Valentin Milea <valentin.milea@gmail.com>
2021-10-03 15:11:35 +0300

    Run slave and master on the same thread

--- i2c_slave_example.c
+++ i2c_slave_example.c
@@ -6,7 +6,6 @@
 
 #include <i2c_fifo.h>
 #include <i2c_slave.h>
-#include <pico/multicore.h>
 #include <pico/stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -60,61 +59,7 @@ static void i2c_slave_handler(i2c_inst_t *i2c, i2c_slave_event_t event) {
     }
 }
 
-static void master_thread() {
-    i2c_init(i2c1, I2C_BAUDRATE);
-
-    gpio_init(I2C_MASTER_SDA_PIN);
-    gpio_set_function(I2C_MASTER_SDA_PIN, GPIO_FUNC_I2C);
-    // pull-ups are already active on slave side, this is a failsafe in case the wiring is faulty
-    gpio_pull_up(I2C_MASTER_SDA_PIN);
-
-    gpio_init(I2C_MASTER_SCL_PIN);
-    gpio_set_function(I2C_MASTER_SCL_PIN, GPIO_FUNC_I2C);
-    gpio_pull_up(I2C_MASTER_SCL_PIN);
-
-    for (uint8_t mem_address = 0; ; mem_address = (mem_address + 32) % 256) {
-        char msg[32];
-        snprintf(msg, sizeof(msg), "Hello, I2C slave! - 0x%02X", mem_address);
-        uint8_t msg_len = strlen(msg);
-        
-        uint8_t buf[32];
-        buf[0] = mem_address;
-        memcpy(buf + 1, msg, msg_len);
-        // write message at mem_address
-        printf("Write at 0x%02X: '%s'\n", mem_address, msg);
-        int result = i2c_write_blocking(i2c1, I2C_SLAVE_ADDRESS, buf, 1 + msg_len, false);
-        if (result < 0) {
-            puts("Couldn't write to slave, please check your wiring!");
-            return;
-        }
-        hard_assert(result == 1 + msg_len);
-
-        // seek to mem_address
-        result = i2c_write_blocking(i2c1, I2C_SLAVE_ADDRESS, buf, 1, false);
-        hard_assert(result == 1);
-        memset(buf, 0, sizeof(buf));
-        // partial read
-        uint8_t split = 5;
-        result = i2c_read_blocking(i2c1, I2C_SLAVE_ADDRESS, buf, split, true);
-        hard_assert(result == split);
-        printf("Read  at 0x%02X: '%s'\n", mem_address, buf);
-        hard_assert(memcmp(buf, msg, split) == 0);
-        // read the remaining bytes, continuing from last address
-        result = i2c_read_blocking(i2c1, I2C_SLAVE_ADDRESS, buf, msg_len - split, false);
-        hard_assert(result == msg_len - split);
-        printf("Read  at 0x%02X: '%s'\n", mem_address + split, buf);
-        hard_assert(memcmp(buf, msg + split, msg_len - split) == 0);
-
-        puts("");
-        sleep_ms(2000);
-    } while (true);
-}
-
-int main() {
-    stdio_init_all();
-    puts("\nI2C slave example");
-
-    // prepare I2C slave
+static void setup_slave() {
     gpio_init(I2C_SLAVE_SDA_PIN);
     gpio_set_function(I2C_SLAVE_SDA_PIN, GPIO_FUNC_I2C);
     gpio_pull_up(I2C_SLAVE_SDA_PIN);
@@ -126,9 +71,63 @@ int main() {
     i2c_init(i2c0, I2C_BAUDRATE);
     // configure I2C0 for slave mode
     i2c_slave_init(i2c0, I2C_SLAVE_ADDRESS, &i2c_slave_handler);
+}
 
-    // start master on the other core
-    multicore_launch_core1(master_thread);
+static void run_master() {
+    gpio_init(I2C_MASTER_SDA_PIN);
+    gpio_set_function(I2C_MASTER_SDA_PIN, GPIO_FUNC_I2C);
+    // pull-ups are already active on slave side, this is just a fail-safe in case the wiring is faulty
+    gpio_pull_up(I2C_MASTER_SDA_PIN);
 
-    sleep_until(at_the_end_of_time);
+    gpio_init(I2C_MASTER_SCL_PIN);
+    gpio_set_function(I2C_MASTER_SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_MASTER_SCL_PIN);
+
+    i2c_init(i2c1, I2C_BAUDRATE);
+
+    for (uint8_t mem_address = 0;; mem_address = (mem_address + 32) % 256) {
+        char msg[32];
+        snprintf(msg, sizeof(msg), "Hello, I2C slave! - 0x%02X", mem_address);
+        uint8_t msg_len = strlen(msg);
+
+        uint8_t buf[32];
+        buf[0] = mem_address;
+        memcpy(buf + 1, msg, msg_len);
+        // write message at mem_address
+        printf("Write at 0x%02X: '%s'\n", mem_address, msg);
+        int count = i2c_write_blocking(i2c1, I2C_SLAVE_ADDRESS, buf, 1 + msg_len, false);
+        if (count < 0) {
+            puts("Couldn't write to slave, please check your wiring!");
+            return;
+        }
+        hard_assert(count == 1 + msg_len);
+
+        // seek to mem_address
+        count = i2c_write_blocking(i2c1, I2C_SLAVE_ADDRESS, buf, 1, true);
+        hard_assert(count == 1);
+        // partial read
+        uint8_t split = 5;
+        count = i2c_read_blocking(i2c1, I2C_SLAVE_ADDRESS, buf, split, true);
+        hard_assert(count == split);
+        buf[count] = '\0';
+        printf("Read  at 0x%02X: '%s'\n", mem_address, buf);
+        hard_assert(memcmp(buf, msg, split) == 0);
+        // read the remaining bytes, continuing from last address
+        count = i2c_read_blocking(i2c1, I2C_SLAVE_ADDRESS, buf, msg_len - split, false);
+        hard_assert(count == msg_len - split);
+        buf[count] = '\0';
+        printf("Read  at 0x%02X: '%s'\n", mem_address + split, buf);
+        hard_assert(memcmp(buf, msg + split, msg_len - split) == 0);
+
+        puts("");
+        sleep_ms(2000);
+    }
+}
+
+int main() {
+    stdio_init_all();
+    puts("\nI2C slave example");
+
+    setup_slave();
+    run_master();
 }
